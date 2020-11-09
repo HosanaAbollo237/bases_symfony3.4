baca7bd73373953d460b0a5aae7ffa5c014336b0
HosanaAbollo237 <hosana.abollo237@gmail.com>
2020-11-09 16:45:14 +0100

    extend and update entity

--- autotrader/src/CarBundle/Entity/Car.php
+++ autotrader/src/CarBundle/Entity/Car.php
@@ -41,6 +41,86 @@ class Car
      * @ORM\Column(name="description", type="text", nullable=true)
      */
     private $description;
+    
+    /**
+     * @var int
+     * 
+     * @ORM\Column(name="price", type="integer")
+     */
+     private $price;
+
+
+    /**
+     * @var boolean
+     * 
+     * @ORM\Column(name="navigation", type="boolean")
+     */
+     private $navigation;
+
+    /**
+     * @var int
+     * 
+     * @ORM\Column(name="year", type="integer")
+     */
+      private $year;
+
+     /**
+      * Get price
+      * 
+      * @return int
+      */
+      public function getPrice(){
+          return $this->price;
+      }
+
+      /**
+       * Set price
+       * 
+       * @param int $price
+       */
+       public function setPrice($price){
+           $this->price = $price;
+           return $this;
+       }
+
+       /**
+        * @return int 
+        *
+        */
+        public function getNavigation(){
+            return $this->navigation;
+        }
+
+        /**
+         * Set Navigation
+         * 
+         * @param int $navigation
+         * 
+         */
+        public function setNavigation($navigation){
+            $this->navigation = $navigation;
+            return $this;
+        }
+
+       /**
+        * Get year
+        * 
+        * @return int
+        */
+        public function getYear(){
+            return $this->year;
+        }
+
+        /**
+         *  Set Year
+         * 
+         *@param int $year
+         */
+         public function setYear($year){
+             $this->year = $year;
+             return $this;
+         }
+
 
     /**
      * Set description
--- autotrader/src/CarBundle/car.sql
+++ autotrader/src/CarBundle/car.sql
@@ -1,5 +1,5 @@
-insert into car(id, name, make) 
-values ('', 'z3', 'OPEL'),
-values ('', 'z3', 'OPEL'),
-values ('', 'z3', 'OPEL'),
-values ('', 'z3', 'OPEL');
+insert into car(id, name, make, description) 
+values ('', 'z3', 'OPEL', "Ce modèle a fait fureur en 2014"),
+values ('', 'z3', 'BMW', "Rien n'est comparable à cette merveille"),
+values ('', '445', 'Renault', "Très utile pour une famille nombreuse"),
+values ('', 'Picasso', 'Fiat', "Pour les artistes un peu déconnectés");
