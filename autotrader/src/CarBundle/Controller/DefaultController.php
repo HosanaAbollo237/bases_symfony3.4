773e0cfc9d10e4452677cd5a0e0627e20b68dc40
HosanaAbollo237 <hosana.abollo237@gmail.com>
2020-11-16 18:05:03 +0100

    envoie + validation form

--- autotrader/src/CarBundle/Controller/DefaultController.php
+++ autotrader/src/CarBundle/Controller/DefaultController.php
@@ -4,24 +4,39 @@ namespace CarBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
+
 
 class DefaultController extends Controller
 {   
     /**
      * @Route("/our-cars/", name="offer")
      */
-    public function indexAction()
+    public function indexAction(Request $request)
     {
         $carRepository = $this->getDoctrine()->getRepository('CarBundle:Car');
         $cars = $carRepository->findCarWithDetails();
 
         $form = $this->createFormBuilder()
         ->setMethod('GET')
-        ->add('search', TextType::class)
+        ->add('search', TextType::class, [
+            'constraints' => [
+                new NotBlank(), // champ non vide
+                new Length([
+                    'min' => 2,
+                    'minMessage' => 'La longueur du champ doit dépasser 2 caractères'
+                    ]) // minimum 2 characters
+            ]
+        ])
         ->getForm();
-
+        $form->handleRequest($request); // recupérer la requete du formulaire
+        
+        if($form->isSubmitted() && $form->isValid()){
+            die("FORM SUBMITTED");
+        }
         return $this->render('CarBundle/default/index.html.twig', [
             'cars' => $cars,
             'form' => $form->createView()
